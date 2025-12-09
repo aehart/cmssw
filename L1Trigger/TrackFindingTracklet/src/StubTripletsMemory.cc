9b3e15c37c4be0c8ad2c040e746c649d2c711e9f
Andrew Hart <ahart@cern.ch>
2025-12-09 22:23:46 +0100

    More consistently use MemoryBase::openFile().

--- L1Trigger/TrackFindingTracklet/src/StubTripletsMemory.cc
+++ L1Trigger/TrackFindingTracklet/src/StubTripletsMemory.cc
@@ -11,15 +11,9 @@ StubTripletsMemory::StubTripletsMemory(string name, Settings const& settings) : 
 
 void StubTripletsMemory::writeST(bool first, unsigned int iSector) {
   iSector_ = iSector;
+
   const string dirSP = settings_.memPath() + "StubPairs/";
-
-  std::ostringstream oss;
-  oss << dirSP << "StubTriplets_" << getName() << "_" << std::setfill('0') << std::setw(2) << (iSector_ + 1) << ".dat";
-  auto const& fname = oss.str();
-
-  openfile(out_, first, dirSP, fname, __FILE__, __LINE__);
-
-  out_ << "BX = " << (bitset<3>)bx_ << " Event : " << event_ << endl;
+  openFile(first, dirSP, "StubTriplets_");
 
   for (unsigned int j = 0; j < stubs1_.size(); j++) {
     string stub1index = stubs1_[j]->stubindex().str();
